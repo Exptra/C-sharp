5642b1b1010d5bbcae634c04059f3bdc5ef16d07
Rizky Ekasaputra <34717157+Exptra@users.noreply.github.com>
2019-08-28 09:55:37 +0700

    Update sum Prime Numbers.cs

--- sum Prime Numbers.cs
+++ sum Prime Numbers.cs
@@ -5,24 +5,24 @@ public class Program
 	public static void Main()
 	{
 			
-			int l,r, sum = 0;
+		int l,r, sum = 0;
 			
-			Console.WriteLine("Enter ");
-            string a = Console.ReadLine();
-			string[] values = a.Split(' ');
+		Console.WriteLine("Enter ");
+           	string a = Console.ReadLine();
+		string[] values = a.Split(' ');
 		
-			l = int.Parse(values[0]);
-			r = int.Parse(values[1]);
+		l = int.Parse(values[0]);
+		r = int.Parse(values[1]);
 			
-			int container; // initialial var penampung
-			for (int i = l+1; i < r; i++) // ambil dari inputan ++
-			{		container = 0; // set value var penampung
-				for (int j = 1; j <= i ; j++) // jika var j dinisialisasi = 0; dia tidak bisa mengeksekusi (Attempted to divide by zero)
+		int container; // initialial var penampung
+		for (int i = l+1; i < r; i++) // ambil dari inputan ++
+		{		container = 0; // set value var penampung
+			for (int j = 1; j <= i ; j++) // 1 2 3 4 ... dia tidak bisa mengeksekusi (Attempted to divide by zero)
+			{
+				if (i%j == 0) 
 				{
-						if (i%j == 0) 
-						{
-							container = container +1;   // save // or container ++;
-						} 
+					container = container +1;   // save // or container ++;
+					} 
 				}
 				if (container == 2){
 					sum += i;
