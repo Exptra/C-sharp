ba7d44dd285b5aefe125b8392b6cbf819c8b9af5
Exptra <34717157+Exptra@users.noreply.github.com>
2019-09-06 11:29:27 +0700

    Update String operation 3.cs

--- String operation 3.cs
+++ String operation 3.cs
@@ -9,7 +9,7 @@ public class Exercise9
  	consonant = 0; // [i]
 		
 	Console.WriteLine("Berapa Karakter ?");
-    int Bkarakter = Convert.ToInt16(Console.ReadLine());
+    	int Bkarakter = Convert.ToInt16(Console.ReadLine());
 	for (int j=0; j<Bkarakter; j++) {
 		vokal = 0; // [i]
  		consonant = 0; // [i]
@@ -17,18 +17,18 @@ public class Exercise9
    		str = Console.ReadLine();			
     		for(int i=0; i<str.Length; i++)
     		{
-       			if(str[i] =='a' || str[i]=='i' || str[i] == 'u' || str[i] == 'e' || str[i] == 'o' || str[i] == 'A' || str[i] == 'I' || str[i] == 'U' || str[i] == 'E' || str[i] == 'O')        
-        		{
-            		vokal++; // jika string[i] == 'a.i.u.e.o'
-         		}
-				else if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z')) // ngecek.menghitung string A-Z jika string[i] tidak ada vokal lalu dihitung 
-        		{
+       		if(str[i] =='a' || str[i]=='i' || str[i] == 'u' || str[i] == 'e' || str[i] == 'o' || str[i] == 'A' || str[i] == 'I' || str[i] == 'U' || str[i] == 'E' || str[i] == 'O')        
+        	{
+            	vokal++; // jika string[i] == 'a.i.u.e.o'
+         	}
+			else if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z')) // ngecek.menghitung string A-Z jika string[i] tidak ada vokal lalu dihitung 
+        	{
             	consonant++;  // jika string[i] == 'selain a.i.u.e.o'
-        		}
+        	}
     	}
-		Console.WriteLine("Jumlah huruf vokal : {0}", vokal);
-		Console.WriteLine("Jumlah huruf konsonan : {0}", consonant);
-		Console.WriteLine();
-		}
+	Console.WriteLine("Jumlah huruf vokal : {0}", vokal);
+	Console.WriteLine("Jumlah huruf konsonan : {0}", consonant);
+	Console.WriteLine();
 	}
+    }
 }
